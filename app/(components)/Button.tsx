1f68989eda853ddf0a161b6b7a6df0a292853304
JoaoVitorWinter <joaovwinter3108@gmail.com>
2024-03-19 21:57:47 -0300

    19/03 21:57

--- app/(components)/Button.tsx
+++ app/(components)/Button.tsx
@@ -5,7 +5,7 @@ type ButtonProps = {
 };
 export default function Button({ children, className, onClick }: ButtonProps) {
   return (
-    <button onClick={onClick} className={`box-border border-2 bg-blue-800 px-4 py-2 text-white transition
+    <button onClick={onClick} className={`box-border border-2 border-transparent bg-blue-800 px-4 py-2 text-white transition
     hover:border-blue-800 hover:bg-white  hover:text-blue-800
     ${className}`}>
       {children}
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,24 +1,48 @@
 'use client'
 
-import { useMemo, useState } from "react";
+import React, { ButtonHTMLAttributes, useEffect, useMemo, useState } from "react";
 import { getCityWeather } from "../(api)/Weather";
 import Button from "../(components)/Button";
 
 export default function Page() {
-    const [cityData, setCityData] = useState();
+    const [cityData, setCityData] = useState("");
+    const [cityName, setCityName] = useState("");
 
+    useEffect(() => {
+        console.log(cityName);
+    }, [cityName])
     const searchedCity = useMemo(() => {
         return <section></section>
     }, [cityData])
 
     const searchCity = async (city: string) => {
-        await getCityWeather(city);
+        try {
+            fetch(`http://api.weatherapi.com/v1/current.json?key=3b67834381c14eb3b2d222433231107&q=${city}`)
+            .then(res => {
+                let weather = res.json();
+                console.log(weather)
+                return weather;
+            })
+            .then(res => console.log(res.location.name))
+            .catch(e =>  console.log(e));
+        } catch (e) {
+            console.error(e);
+        } finally {
+            setCityName("");
+        }
         
     }
+
+    const handleKeyDown = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCityName(e.target.value);
+    }
+
     return (
-        <main className="grid w-full grid-flow-row grid-cols-6 items-center justify-center gap-8 md:grid-cols-12">
-            <input type="text" id="searchInput" className={`col-start-4 col-end-9 px-4 py-2 mt-10`} placeholder={"Write the name city"}/>
-            <button onClick={() => searchCity("London")}>click</button>
+        <main className="grid w-full grid-flow-row grid-cols-6 items-center justify-center gap-8 md:grid-cols-12 mt-10">
+            <section className="col-span-full flex gap-1 justify-center">
+            <input onChange={handleKeyDown} value={cityName} type="text" id="searchInput" className={`w-[50%] px-4 py-2`} placeholder={"Write the name city"}/>
+            <Button className={''} onClick={() => searchCity(cityName)}>Search</Button>
+            </section>
             {searchedCity}
         </main>
     );
